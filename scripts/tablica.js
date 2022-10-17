a833aa8f4afca035cddc55ace3d9da8e89724a5b
stoodnia <k.studniarek.414@studms.ug.edu.pl>
2022-10-17 15:26:20 +0200

    wpisywanie pierwiastkow

---
+++ scripts/tablica.js
@@ -0,0 +1,304 @@
+export const tablicaPierwiastkow = [
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 2,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    {
+        c: 1,
+        r: 1,
+        name: 'wodór', 
+        symbol: "H"
+    },
+    
+
+]
